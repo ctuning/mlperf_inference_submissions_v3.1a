0b66976eacc66e7a67d3eea7c760a34df3caef74
Anandhu S <anandhukicks@gmail.com>
2023-07-08 20:11:15 +0000

    Mobilenet results added

---
+++ open/cTuning/systems/tmp-run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export CM_GIT_CHECKOUT_PATH="/root/CM/repos/local/cache/6a6b1be6d1aa490b/repo"
+export CM_GIT_REPO_CHECKOUT=""
+export CM_GIT_REPO_CHECKOUT_FOLDER="repo"
+export CM_GIT_REPO_CHECKOUT_PATH="/root/CM/repos/local/cache/6a6b1be6d1aa490b/repo"
+export CM_GIT_REPO_DEPTH="--depth 4"
+export CM_GIT_REPO_NAME="mlperf_inference_submissions_v3.1"
+export CM_GIT_REPO_PATCH="no"
+export CM_GIT_REPO_RECURSE_SUBMODULES=" --recurse-submodules"
+export CM_GIT_REPO_URL="https://github.com/anandhu-eng/mlperf_inference_submissions_v3.1"
+export CM_MLPERF_RESULTS_GIT_REPO_URL="https://github.com/anandhu-eng/mlperf_inference_submissions_v3.1"
+export CM_MLPERF_RESULTS_REPO_COMMIT_MESSAGE="Mobilenet results added"
+export CM_MLPERF_SUBMISSION_DIR="/root/mobilenet_submission_tree"
+export CM_PYTHON_BIN="python3"
+export CM_PYTHON_BIN_PATH="/usr/bin"
+export CM_PYTHON_BIN_WITH_PATH="/usr/bin/python3"
+export CM_PYTHON_CACHE_TAGS="version-3.10.6,non-virtual"
+export CM_PYTHON_VERSION="3.10.6"
+export CM_TMP_CURRENT_PATH="/root/mobilenet_submission_tree/open/cTuning/systems"
+export CM_TMP_CURRENT_SCRIPT_PATH="/root/CM/repos/mlcommons@ck/cm-mlops/script/push-mlperf-inference-results-to-github"
+export CM_TMP_PIP_VERSION_STRING=""
+
+
+. /root/CM/repos/mlcommons@ck/cm-mlops/script/push-mlperf-inference-results-to-github/run.sh
