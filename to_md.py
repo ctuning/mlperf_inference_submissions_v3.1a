d82919fee38b1db36760f62f23ee60e4e278bfea
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-07-10 11:32:12 +0100

    Add summary.csv inside README.md

--- to_md.py
+++ to_md.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df=pd.read("summary.csv", engine='python')
+df=pd.read_csv("summary.csv", engine='python')
 
 with open("README.md", "w") as md:
   df.to_markdown(buf=md, tablefmt="grid")
